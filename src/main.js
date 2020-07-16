eb2d53eb4758feebfb97e4d3c2fe9dfd04b94897
Esteban Cardona <ecardona@lan.hch.cl>
2020-07-16 15:18:08 -0400

    Configuraciones hasta la clase 9

--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
+// Paquetes de npm
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+// Archivos locales de nuestra App
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// CSS global
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Configuración extra
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  methods: {
+    // Nuestra función
+    init () {
+      store.dispatch('oauth/getToken', null, { root: true })
+    }
+  },
+  created () {
+    this.init()
+  },
   render: h => h(App)
 }).$mount('#app')
