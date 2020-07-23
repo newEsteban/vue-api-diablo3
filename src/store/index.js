6ed0a1f4fc4469ca3388b21ea8a2964703bd2764
Esteban Cardona <ecardona@lan.hch.cl>
2020-07-22 20:55:52 -0400

    Vue router y profiles

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,13 @@ import Vuex from 'vuex'
 // Importar oauth
 import oauth from './modules/oauth'
 import loading from './modules/loading'
+import error from './modules/error'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     oauth,
-    loading
+    loading,
+    error
   }
 })
